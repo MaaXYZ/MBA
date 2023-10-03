8f78bdde71d216493372801761df78ecbdb26c95
Dissectum <124477333+Dissectum@users.noreply.github.com>
2023-10-04 00:09:31 +0800

    fix: 修复每次启动都创建日志文件的问题

--- src/MBA.Core/Managers/LogManager.cs
+++ src/MBA.Core/Managers/LogManager.cs
@@ -1,6 +1,8 @@
 using MaaToolKit.Extensions;
 using MBA.Core.Data;
 using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace MBA.Core.Managers;
 
@@ -8,27 +10,18 @@ public static class LogManager
 {
     public static ILogger Logger { get; private set; } = Serilog.Core.Logger.None;
 
+    private static readonly LoggingLevelSwitch _levelSwitch = new();
+
     static LogManager()
     {
-        ConfigureLogger(true);
-    }
-
-    private static bool _logStarted = false;
-
-    /// <summary>
-    /// Configures the <see cref="Logger"/>.
-    /// </summary>
-    /// <param name="enableDebugMode">Indicating whether degub mode is enabled or not.</param>
-    /// <param name="startLog">Indicating whether start infomation is logged or not.</param>
-    public static void ConfigureLogger(bool enableDebugMode, bool startLog = false)
-    {
-        var env = Environment.GetEnvironmentVariable("MBA_ENVIRONMENT") ?? "Empty";
-        enableDebugMode = enableDebugMode || env == "Debug";
-
+        const string Template = "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}][{Level:u3}] {Message:lj}{NewLine}{Exception}";
         var config = new LoggerConfiguration()
+            .MinimumLevel.ControlledBy(_levelSwitch)
             .WriteTo.File(
                 path: GlobalInfo.LogFileFullPath,
-                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff}][{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                outputTemplate: Template,
+                // levelSwitch: LevelSwitch,
+                restrictedToMinimumLevel: LogEventLevel.Verbose,
                 rollingInterval: RollingInterval.Day,
                 retainedFileCountLimit: 7);
 
@@ -36,13 +29,31 @@ public static class LogManager
             ? config.WriteTo.Console()
             : config.WriteTo.Debug();
 
-        config = enableDebugMode
-            ? config.MinimumLevel.Verbose()
-            : config.MinimumLevel.Information();
-        SetFrameworkLog(enableDebugMode);
-
         Logger = config.CreateLogger();
         Log.Logger = Logger;
+
+        ConfigureLogger(true);
+    }
+
+    /// <summary>
+    ///     Configures the <see cref="Logger"/>.
+    /// </summary>
+    /// <param name="enableDebugMode">Indicating whether degub mode is enabled or not.</param>
+    /// <param name="startLog">Indicating whether start infomation is logged or not.</param>
+    public static void ConfigureLogger(bool enableDebugMode, bool startLog = false)
+    {
+        // Information  - 给用户看的信息
+        // Debug        - 给开发者看的信息
+        // Verbose      - 跟踪信息，开发者不怎么想看的那种
+
+        var env = Environment.GetEnvironmentVariable("MBA_ENVIRONMENT") ?? "Empty";
+        enableDebugMode = enableDebugMode || env == "Debug";
+        SetFrameworkLog(enableDebugMode);
+
+        if (enableDebugMode)
+            _levelSwitch.MinimumLevel = LogEventLevel.Verbose;
+        else
+            _levelSwitch.MinimumLevel = LogEventLevel.Information;
 
         if (startLog)
             LogStart(env, enableDebugMode);
@@ -54,9 +65,12 @@ public static class LogManager
         MaaObject.DebugMode = enableDebugMode;
     }
 
+    private static bool _logStarted = false;
+
     private static void LogStart(string env, bool enableDebugMode)
     {
         if (_logStarted) return;
+        _logStarted = true;
 
         Log.Information("===================================");
         Log.Information("  MBA {UI} v{Version} started", GlobalInfo.IsCli ? "CLI" : "GUI", VersionManager.InformationalVersion);
